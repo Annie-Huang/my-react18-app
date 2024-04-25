243c0598fe5ad2b67a2143fc1e581e504d722e27
Annie Huang <annie.hn@gmail.com>
2024-04-25 20:39:28 +1000

    Fixed unit test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('tab', {
+    name: /Item One/i,
+  });
   expect(linkElement).toBeInTheDocument();
 });
